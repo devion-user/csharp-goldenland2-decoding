a1c7bae6bd515fb0f7333216a3077ef77e8d6ba0
Alex Kasaurov <ak@modumlab.com>
2020-11-03 15:52:53 +0300

    decoding .pak format

---
+++ BurutPack/gl2 full extract (move all files from here in gl2data dir).bat
@@ -0,0 +1,17 @@
+BurutPak.exe x -r -o -y engineres.pak
+BurutPak.exe x -r -o -y engineres.update.3.pak
+BurutPak.exe x -r -o -y items.pak
+BurutPak.exe x -r -o -y items.update.3.pak
+BurutPak.exe x -r -o -y levels.pak
+BurutPak.exe x -r -o -y levels.update.3.pak
+BurutPak.exe x -r -o -y magic.pak
+BurutPak.exe x -r -o -y music.pak
+BurutPak.exe x -r -o -y persons.pak
+BurutPak.exe x -r -o -y scripts.pak
+BurutPak.exe x -r -o -y scripts.update.3.pak
+BurutPak.exe x -r -o -y sdb.pak
+BurutPak.exe x -r -o -y sdb.update.3.pak
+BurutPak.exe x -r -o -y sounds.pak
+BurutPak.exe x -r -o -y sounds.update.3.pak
+BurutPak.exe x -r -o -y wear.pak
+pause
